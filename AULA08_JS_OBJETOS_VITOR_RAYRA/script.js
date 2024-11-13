583129e7c8f85adda3d2f7ce2e31786f81faa4ae
rayravi08 <vitor.rayra123@gmail.com>
2024-11-13 08:50:20 -0300

    Add files via upload

---
+++ AULA08_JS_OBJETOS_VITOR_RAYRA/script.js
@@ -0,0 +1,12 @@
+//trabalho com objetos no javascript
+
+//Criar uma variável do tipo objeto
+let pessoa = {
+  nome: "Deivid",
+  idade: 52,
+  altura: 1.68,
+  cargo: "estudante",
+};
+
+console.log(pessoa);
+console.log(pessoa);
